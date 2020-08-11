62cf6d268361798be02d53a6abe5fd604adc4b0b
Hatch <Tyler.Hatch@water.ca.gov>
2020-08-11 14:06:20 -0700

    Add import for IWFMStratigraphy class

--- pyiwfm/__init__.py
+++ pyiwfm/__init__.py
@@ -1,3 +1,4 @@
 from pyiwfm.core.nodes import IWFMNodes
 from pyiwfm.core.elements import IWFMElements
+from pyiwfm.core.stratigraphy import IWFMStratigraphy
 from pyiwfm.utilities.utilities import *
