d95df8006afd77760a4391e34a1d1f5b1a31953f
Hatch <Tyler.Hatch@water.ca.gov>
2020-09-19 16:01:35 -0700

    Create LinkedListNode class

---
+++ pyiwfm/core/util/Utilities/linked_list_node.py
@@ -0,0 +1,45 @@
+'''
+linked_list_node.py
+Author: Tyler Hatch PhD, PE
+
+This is the linked_list_node  module of the python version of IWFM
+'''
+# local imports
+from message_logger import set_last_message
+from message_logger import FATAL
+
+MODULE_NAME = "LLNode::"
+
+class LinkedListNode:
+    def __init__(self, value):
+        self.__value = value
+        self.__next_value = None
+
+    def __del__(self):
+        pass
+
+    def get_next(self):
+        return self.__next_value.get_value()
+
+    def get_value(self):
+        return self.__value
+
+    def set_next(self, node):
+        this_procedure = MODULE_NAME+'set_next'
+        
+        if not isinstance(node, LinkedListNode):
+            raise TypeError("node must be of type LinkedListNode")
+
+        if self.__next_value is not None:
+            set_last_message("Can only add a node to the end of a linked list.", FATAL, this_procedure)
+        
+        self.__next_value = node
+
+if __name__ == '__main__':
+    n1 = LinkedListNode("Dec")
+    n2 = LinkedListNode("Jan")
+    print(n1.get_value())
+    n1.set_next(n2)
+    print(n1.get_next())
+    n2.set_next("Feb")
+
