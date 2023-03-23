49875200264cfd93f17cf2624e65dd771958247a
Tyler Hatch <hatch.tyler@gmail.com>
2023-03-23 15:30:42 -0700

    Implement time series utility functions

---
+++ pyiwfm/core/util/Utilities/timeseriesutilities.py
@@ -0,0 +1,718 @@
+"""
+timeseries_utilities.py
+Author: Tyler Hatch PhD, PE
+
+This is the timeseries_utilities module of the python version of IWFM
+"""
+from pyiwfm.core.util.Utilities.general_utilities import count_occurence, first_location
+
+from pyiwfm.core.util.Utilities.message_logger import log_message, FATAL
+
+MODNAME = "TimeSeriesUtilities::"
+
+TIMESTEP_LENGTH = 16
+cache_limit = 500
+simulation_timestep_inminutes = 0
+
+
+class TimeStep:
+    def __init__(
+        self,
+        track_time=False,
+        current_time=0.0,
+        end_time=0.0,
+        current_timestep=0,
+        current_date_and_time="",
+        end_date_and_time="",
+        deltat=0.0,
+        deltat_inminutes=0,
+        unit="",
+    ):
+        self.track_time = track_time
+        self.current_time = current_time
+        self.end_time = end_time
+        self.current_timestep = current_timestep
+        self.current_date_and_time = current_date_and_time
+        self.end_date_and_time = end_date_and_time
+        self.deltat = deltat
+        self.deltat_inminutes = deltat_inminutes
+        self.unit = unit
+
+
+RECOGNIZED_INTERVALS_INMINUTES = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    10,
+    15,
+    20,
+    30,
+    60,
+    120,
+    180,
+    240,
+    360,
+    480,
+    720,
+    1440,
+    10080,
+    43200,
+    525600,
+]
+
+RECOGNIZED_INTERVALS = [
+    "1MIN",
+    "2MIN",
+    "3MIN",
+    "4MIN",
+    "5MIN",
+    "10MIN",
+    "15MIN",
+    "20MIN",
+    "30MIN",
+    "1HOUR",
+    "2HOUR",
+    "3HOUR",
+    "4HOUR",
+    "6HOUR",
+    "8HOUR",
+    "12HOUR",
+    "1DAY",
+    "1WEEK",
+    "1MON",
+    "1YEAR",
+]
+
+
+def is_time_interval_valid(interval):
+    """
+    Return the index of the time interval if valid
+    """
+    if interval not in RECOGNIZED_INTERVALS:
+        return -1
+
+    return RECOGNIZED_INTERVALS.index(interval)
+
+
+def is_leap_year(year):
+    """
+    Check if year is a leap year
+    """
+    if year % 400 == 0:
+        return True
+    elif year % 100 == 0:
+        return False
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
+
+
+def julian_date_to_day_month_year(julian_date):
+    """
+    Convert julian date to day month year
+
+    Parameters
+    ----------
+    julian_date : int
+        julian date to convert
+
+    Returns
+    -------
+    tuple
+        day, month, year
+    """
+    l = int(julian_date + 68569)
+    n = int(4 * l / 146097)
+    l = l - int((146097 * n + 3) / 4)
+    year = int(4000 * (l + 1) / 1461001)
+    l = l - int(1461 * year / 4) + 31
+    month = int(80 * l / 2447)
+    day = l - int(2447 * month / 80)
+    l = int(month / 11)
+    month = month + 2 - 12 * l
+    year = 100 * (n - 49) + year + l
+
+    return day, month, year
+
+
+def day_month_year_to_julian_date(day, month, year, return_status=False):
+    """
+    Convert day, month, year to julian day
+
+    this approach can only convert a date after 1 Jan 0001 to julian day
+
+    Parameters
+    ----------
+    day : int
+        integer day of month
+
+    month : int
+        integer month of year
+
+    year : int
+        integer year
+
+    return_status : bool default False
+        flag to determine if status is also returned
+
+    Returns
+    -------
+    int
+        julian date
+    """
+    this_procedure = MODNAME + "DayMonthYearToJulianDate"
+
+    # make sure year is not less than 1
+    if year < 1:
+        log_message(
+            "Cannot convert dates with calendar year less than 1 to Julian day!",
+            FATAL,
+            this_procedure,
+        )
+
+    # make sure month is between 1 and 12
+    if month < 1 or month > 12:
+        log_message(f"Incorrect number for month {month}!", FATAL, this_procedure)
+
+    # make sure day is correct
+    if month in [1, 3, 5, 7, 8, 10, 12]:
+        if day < 1 or day > 31:
+            error_code = 1
+        else:
+            error_code = 0
+
+    elif month in [4, 6, 9, 11]:
+        if day < 1 or day > 30:
+            error_code = 1
+        else:
+            error_code = 0
+
+    else:
+        if is_leap_year(year):
+            if day < 1 or day > 29:
+                error_code = 1
+            else:
+                error_code = 0
+
+        else:
+            if day < 1 or day > 28:
+                error_code = 1
+            else:
+                error_code = 0
+
+    if error_code != 0:
+        log_message(
+            f"Day {day} of the month is incorrect given the month {month}!",
+            FATAL,
+            this_procedure,
+        )
+
+    julian_date = int(
+        day
+        - 32075
+        + 1461 * (year + 4800 + (month - 14) / 12) / 4
+        + 367 * (month - 2 - (month - 14) / 12 * 12) / 12
+        - 3 * ((year + 4900 + (month - 14) / 12) / 100) / 4
+    )
+
+    if not return_status:
+        return julian_date
+
+    return julian_date, error_code
+
+
+def leap_year_correction(timestamp):
+    """
+    Correct timestamp for leap year
+
+    Parameters
+    ----------
+    timestamp : str
+        timestamp to check and correct for leap year
+
+    Returns
+    -------
+    str
+        corrected timestamp accounting for leap year
+    """
+    this_procedure = MODNAME + "LeapYearCorrection"
+
+    # set output timestamp equal to timestamp
+    out_timestamp = timestamp
+
+    # check if timestamp refers to February
+    if extract_month(timestamp) != 2:
+        return timestamp
+
+    # check if timestamp refer February 29
+    if extract_day(timestamp) != 29:
+        return timestamp
+
+    # get the year
+    year = extract_year(timestamp)
+
+    # make sure that year of timestamp does not use the 4000 flag for the year
+    if year == 4000:
+        message_array = []
+        message_array.append(f"Time stamp {timestamp} cannot be corrected for ")
+        message_array.append("leap year because it includes the Year 4000 flag!")
+        log_message(message_array, FATAL, this_procedure)
+
+    # correct February 29 if it is a leap year
+    if not is_leap_year(year):
+        return timestamp.replace("/29/", "/28/")
+
+
+def is_timestamp_valid(aline):
+    """
+    Check if a string is a valid timestamp
+
+    Parameters
+    ----------
+    aline : str
+        value to check if valid timestamp
+
+    Returns
+    -------
+    bool
+        True if string is valid timestamp else False
+    """
+    # strip timestamp from string
+    timestamp = strip_timestamp(aline)
+
+    # check if propective timestamp has 2 "/"
+    if count_occurence("/", timestamp) != 2:
+        return False
+
+    # check if prospective timestamp has 1 "_"
+    if count_occurence("_", timestamp) != 1:
+        return False
+
+    # check if prospective timestamp has 1 ":"
+    if count_occurence(":") != 1:
+        return False
+
+    # check if length is equal to the timestep length
+    if len(timestamp.strip()) != TIMESTEP_LENGTH:
+        return False
+
+    return True
+
+
+def strip_timestamp(aline):
+    """
+    Strips a timestamp from a string
+
+    Parameters
+    ----------
+    aline : str
+        string containing timestamp to extract
+
+    Returns
+    -------
+    str or None
+        timestamp
+    """
+    # check if the line includes "/"
+    location_of_first_slash = first_location("/", aline)
+
+    #
+    if not location_of_first_slash:
+        return None
+
+    # beginning of timestamp should be two characters before the first slash
+    # otherwise the first character of the string
+    timestamp_begin = max(location_of_first_slash - 2, 0)
+
+    # end of timestamp should be the timestep length past the beginning of the
+    # timestamp but no less than the beginning of the timestamp
+    timestamp_end = max(timestamp_begin + TIMESTEP_LENGTH, timestamp_begin)
+
+    # slice timestamp from string line
+    timestamp = aline[timestamp_begin, timestamp_end]
+
+    return timestamp
+
+
+def extract_month(timestamp):
+    """
+    Extract month from timestamp string
+
+    Parameters
+    ----------
+    timestamp : str
+        timestamp to extract month
+
+    Returns
+    -------
+    int
+        integer month from timestamp
+    """
+    return int(timestamp[:2])
+
+
+def extract_day(timestamp):
+    """
+    Extract day from timestamp string
+
+    Parameters
+    ----------
+    timestamp : str
+        timestamp to extract day
+
+    Returns
+    -------
+    int
+        integer day from timestamp
+    """
+    return int(timestamp[3:5])
+
+
+def extract_year(timestamp):
+    """
+    Extract year from timestamp string
+
+    Parameters
+    ----------
+    timestamp : str
+        timestamp to extract year
+
+    Returns
+    -------
+    int
+        integer year from timestamp string
+    """
+    return int(timestamp[6:10])
+
+
+def extract_hour(timestamp):
+    """
+    Extract hour from timestamp string
+
+    Parameters
+    ----------
+    timestamp : str
+        timestamp to extract hour
+
+    Returns
+    -------
+    int
+        integer hour from timestamp string
+    """
+    return int(timestamp[11:13])
+
+
+def extract_minute(timestamp):
+    """
+    Extract minute from timestamp string
+
+    Parameters
+    ----------
+    timestamp : str
+        timestamp to extract minute
+
+    Returns
+    -------
+    int
+        integer minute from timestamp string
+    """
+    return int(timestamp[14:])
+
+
+def timestamp_to_julian_date_and_minutes(timestamp, return_status=False):
+    """
+    Extract julian date and minutes past midnight from timestamp
+
+    Parameters
+    ----------
+    timestamp : str
+        timestamp to convert
+
+    Returns
+    -------
+    tuple[int, int]
+        if return_status = False, julian date and number of minutes past midnight
+    
+    tuple[int, int, int]
+        if return_status = True, julian date, number of minutes past midnight, and status
+    """
+    this_procedure = MODNAME + "TimeStampToJulianDateAndMinutes"
+
+    # convert date to julian date
+    julian_date, error_code = day_month_year_to_julian_date(
+        extract_day(timestamp),
+        extract_month(timestamp),
+        extract_year(timestamp),
+        return_status=True,
+    )
+
+    # if return_status is True, then return the status code otherwise throw error
+    # when error_code is not equal to 0 (success)
+    if not return_status and error_code != 0:
+        log_message(
+            "Error in converting simulation date to Julian date",
+            FATAL,
+            this_procedure,
+        )
+        return
+    
+    status = error_code
+
+    minutes_after_midnight = extract_hour(timestamp) * 60 + extract_minute(timestamp)
+
+    if return_status:
+        return julian_date, minutes_after_midnight, status
+    
+    return julian_date, minutes_after_midnight
+
+
+def dssstyledate_from_daymonthyear(day, month, year):
+    """
+    Convert day, month, year to dss style date
+
+    Parameters
+    ----------
+    day : int
+        integer day
+
+    month : int
+        integer month
+
+    year : int
+        integer year
+
+    Returns
+    -------
+    str
+        dss style date
+    """
+    months = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+    ]
+
+    return f"{day:02d}{months[month-1]}{year}"
+
+
+def dssstyledate_from_timestamp(timestamp):
+    """
+    Convert timestamp to DSS-style date
+
+    Parameters
+    ----------
+    timestamp : str
+        timestamp to convert to DSS-style date
+
+    Returns
+    -------
+    str
+        dss style date
+    """
+    months = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+    ]
+
+    day = extract_day(timestamp)
+    month = extract_month(timestamp)
+    year = extract_year(timestamp)
+
+    return f"{day} {months[month-1]} {year}"
+
+
+def dssstylehours_aftermidnight(timestamp):
+    """
+    Get DSS style hours after midnight from timestamp
+
+    Parameters
+    ----------
+    timestamp : str
+        timestamp to extract DSS-style hours after midnight
+
+    Returns
+    -------
+    str
+        DSS-style hours after midnight
+    """
+    hours = extract_hour(timestamp)
+    minutes = extract_minute(timestamp)
+
+    return f"{hours}{minutes}"
+
+
+def julian_date_and_minutes_to_timestamp(julian_date, minutes_after_midnight):
+    """
+    Convert julian date and minutes after midnight to timestamp
+
+    Parameters
+    ----------
+    julian_date : int
+        julian date to convert to timestamp
+
+    minutes_after_midnight : int
+        number of minutes after midnight
+
+    Return
+    ------
+    str
+        timestamp
+    """
+    hours = int(minutes_after_midnight / 60)
+    minutes = minutes_after_midnight - hours * 60
+
+    if hours == 0 and minutes == 0:
+        hours = 24
+        temp_julian_date = julian_date - 1
+    else:
+        temp_julian_date = julian_date
+
+    day, month, year = julian_date_to_day_month_year(temp_julian_date)
+
+    return f"{month:02d}/{day:02d}/{year}_{hours}:{minutes:02d}"
+
+
+def timestamp_to_julian(timestamp, return_status=False):
+    """
+    Convert timestamp to fractional julian time
+
+    Parameters
+    ----------
+    timestamp : str
+        time stamp of date and time
+
+    Returns
+    -------
+    float
+        if return_status is False, julian date including fractional day
+
+    tuple[float, int]
+        if return_status is True, julian date including fractional day and status
+    """
+    julian_date, minutes_after_midnight, status = timestamp_to_julian_date_and_minutes(
+        timestamp, return_status=True
+    )
+    
+    julian = float(julian_date) + minutes_after_midnight/1440
+    
+    if return_status:
+        return julian, status
+    
+    return julian
+
+
+def julian_to_timestamp(julian):
+    """
+    Convert fractional julian time to timestamp
+    
+    Parameters
+    ----------
+    julian : float
+        fractional julian date and time
+
+    Returns
+    -------
+    str
+        timestamp
+    """
+    # get full julian day representation of julian date
+    julian_date = int(julian)
+
+    # get number of minutes after midnight
+    minutes_after_midnight = int(1440 * (julian - julian_date))
+
+    return julian_date_and_minutes_to_timestamp(julian_date, minutes_after_midnight)
+
+
+def get_julian_dates_between_timestamps_with_time_increment():
+    pass
+
+
+def adjust_timestamp_with_year_4000():
+    pass
+
+
+def timestamp_to_year_4000():
+    pass
+
+
+def increment_timestamp():
+    pass
+
+
+def increment_julian_date_and_minutes_after_midnight():
+    pass
+
+
+def set_simulation_timestep():
+    pass
+
+
+def set_cache_limit():
+    pass
+
+
+def get_cache_limit():
+    pass
+
+
+def set_tsd_cache_size():
+    pass
+
+
+def adjust_rate_type_data():
+    pass
+
+
+def time_units_check_less_than_or_equal():
+    pass
+
+
+def check_for_less_than():
+    pass
+
+
+def check_for_greater_than():
+    pass
+
+
+def check_for_greater_than_or_equal_to():
+    pass
+
+
+def n_periods_between_timestamps():
+    pass
+
+
+def n_periods_between_times():
+    pass
+
+
+def ctimestep_to_rtimestep():
+    pass
+
+
+def time_interval_conversion():
+    pass
